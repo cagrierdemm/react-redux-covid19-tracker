27feab1c6b016cf2913e77943b1136aa9ea63e51
Çağrı Erdem <cagrierdem001@gmail.com>
2023-12-23 14:49:55 +0300

    third commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ function Covid() {
     const countries = useSelector((state) => state.covid.items.result);
     const total = useSelector((state) => state.total.items);
     const status = useSelector(state => state.covid.status);
-    const [selectedCountry, setSelectedCountry] = useState(0);
+    const [selectedCountry, setSelectedCountry] = useState(-1);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -26,7 +26,6 @@ function Covid() {
     </div></div>
 
 
-    // Check if countries array is undefined or empty
     if (!countries || countries.length === 0) {
         return <div>No data available</div>;
     }
@@ -34,21 +33,21 @@ function Covid() {
     const data = [
         {
           quarter: 1,
-          person: parseInt(countries[selectedCountry]?.totalCases.replace(/,/g, ''), 10) || 0,
-          label: countries[selectedCountry]?.totalCases || "N/A"
+          person: selectedCountry === -1 ? (parseInt(total.result.totalCases.replace(/,/g, ''), 10) || 0) : (parseInt(countries[selectedCountry]?.totalCases.replace(/,/g, ''), 10) || 0),
+          label: selectedCountry === -1 ? total.result.totalCases : countries[selectedCountry]?.totalCases || "N/A"
         },
         {
           quarter: 2,
-          person: parseInt(countries[selectedCountry]?.totalDeaths.replace(/,/g, ''), 10) || 0,
-          label: countries[selectedCountry]?.totalDeaths || "N/A"
+          person: selectedCountry === -1 ? (parseInt(total.result.totalDeaths.replace(/,/g, ''), 10) || 0) : (parseInt(countries[selectedCountry]?.totalDeaths.replace(/,/g, ''), 10) || 0),
+          label: selectedCountry === -1 ? total.result.totalDeaths : countries[selectedCountry]?.totalDeaths || "N/A"
         },
         {
           quarter: 3,
-          person: parseInt(countries[selectedCountry]?.totalRecovered.replace(/,/g, ''), 10) || 0,
-          label: countries[selectedCountry]?.totalRecovered || "N/A"
+          person: selectedCountry === -1 ? (parseInt(total.result.totalRecovered.replace(/,/g, ''), 10) || 0) : (parseInt(countries[selectedCountry]?.totalRecovered.replace(/,/g, ''), 10) || 0),
+          label: selectedCountry === -1 ? total.result.totalRecovered : countries[selectedCountry]?.totalRecovered || "N/A"
         },
       ];
-
+      
 
     return (
         <div className='container'>
@@ -59,12 +58,11 @@ function Covid() {
                             <img src="./covid.png" className="img-fluid" alt="Covid Logo" />
                         </div>
                         <div className="col-2 d-flex align-items-center justify-content-center">
-                           <a href='https://github.com/cagrierdemm'> <img src="./github.png" className="img-fluid hover" alt="GitHub Logo" style={{ height: "32px", width: "32px" }} /></a>
+                           <a href='https://github.com/cagrierdemm'> <img src="./github.png" className="hover" alt="GitHub Logo" style={{ height: "32px", width: "32px" }} /></a>
                         </div>
                     </div>
                 </div>
             </div>
-
 
             <div className='row mt-4'>
                 <div className='mx-auto'>
@@ -73,34 +71,42 @@ function Covid() {
                             <div className='row col-md-12 col-lg-8 d-flex justify-content-center mx-auto'>
                                 <div className="card text-center col-md-3 mb-3 mx-2 mb-md-0 bg-info border-0 border-bottom text-white">
                                     <h5 className="card-body ">
-                                        <b>Total Cases</b>
+                                        <b>{selectedCountry === -1 ? "Total Cases" : "Cases"}</b>
                                     </h5>
                                     <div className="card-body my-4">
-                                        <h3 className="card-title mb-5">{total.result.totalCases}</h3>
+                                        <h3 className="card-title mb-5">{
+                                            selectedCountry === -1 ? total.result.totalCases : countries[selectedCountry].totalCases
+                                        }</h3>
                                     </div>
 
                                 </div>
                                 <div className="card text-center col-md-3 mb-3 mx-2 mb-md-0 border-0 bg-danger text-white">
                                     <h5 className="card-body">
-                                        <b>Total Deaths</b>
+                                        <b>{selectedCountry === -1 ? "Total Deaths" : "Deaths"}</b>
                                     </h5>
                                     <div className="card-body my-4">
-                                        <h3 className="card-title mb-5">{total.result.totalDeaths}</h3>
+                                        <h3 className="card-title mb-5">{
+                                            selectedCountry === -1 ? total.result.totalDeaths : countries[selectedCountry].totalDeaths
+                                        }</h3>
                                     </div>
                                 </div>
                                 <div className="card text-center col-md-3 mb-3 mx-2 mb-md-0 border-0 bg-opacity-75 bg-success text-white">
                                     <h5 className="card-body">
-                                        <b>Total Recovered</b>
+                                        <b>{selectedCountry === -1 ? "Total Recovered" : "Recovered"}</b>
                                     </h5>
                                     <div className="card-body my-4">
-                                        <h3 className="card-title mb-5">{total.result.totalRecovered}</h3>
+                                        <h3 className="card-title mb-5">{
+                                            selectedCountry === -1 ? total.result.totalRecovered : countries[selectedCountry].totalRecovered
+                                        }</h3>
                                     </div>
                                 </div>
                             </div>
                             <div className='row col-md-9 col-lg-6 col-12 mx-auto mt-4'>
                                 <h3>Select Country:</h3>
-                                <select className="form-select mt-2" aria-label="Default select example" onChange={(e) => setSelectedCountry(e.target.selectedIndex)}>{countries.map((i, k) => (
-                                    <option key={k} value={i.country}>{i.country}</option>
+                                <select className="form-select mt-2" aria-label="Default select example" onChange={(e) => setSelectedCountry(e.target.selectedIndex-1)}>
+                                <option key={-1}>All</option>
+                                    {countries.map((i, k) => (
+                                    <option key={k-1} value={i.country}>{i.country}</option>
                                 ))}</select>
                                 <div className='mx-auto col-lg-9 col-md-9 col-12'>
                                     <VictoryChart
